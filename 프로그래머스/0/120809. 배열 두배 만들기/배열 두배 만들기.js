e983dd0d838f7b058965ac4e278638e4f896d619
YeBin0711 <151003669+YeBin0711@users.noreply.github.com>
2024-08-31 17:41:13 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.js
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -5,3 +5,11 @@ function solution(numbers) {
     })
     return answer;
 }
+
+//reduce를 사용해서 작성
+function solution1(numbers) {
+    return numbers.reduce((a, b) => [...a, b * 2], []);
+}
+
+//map을 사용해서 작성
+const solution2 = (numbers) => numbers.map((number) => number * 2)
