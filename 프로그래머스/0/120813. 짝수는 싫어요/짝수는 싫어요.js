e3deeeff5f76858a40a6930686ae6d26930500d8
YeBin0711 <151003669+YeBin0711@users.noreply.github.com>
2024-08-31 18:16:28 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -7,3 +7,12 @@ function solution(n) {
     }
     return answer;
 }
+
+//+2를 사용하기
+function solution2(n) {
+    var answer = [];
+
+    for (let i = 1; i<=n; i+=2) answer.push(i)
+
+    return answer;
+}
