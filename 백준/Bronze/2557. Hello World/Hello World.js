0ae6742dd788bd83baa7390848faf4a9b95eb1e4
YeBin0711 <151003669+YeBin0711@users.noreply.github.com>
2024-10-10 18:30:48 +0900

    [Bronze V] Title: Hello World, Time: 100 ms, Memory: 9432 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.js
@@ -0,0 +1,16 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [];
+
+readline.on('line', function(line) {
+    input = line.split(' ').map(el => parseInt(el));
+}).on('close', function(){
+	/*
+		솔루션 작성
+	*/
+    console.log('Hello World!');
+    process.exit();
+});
