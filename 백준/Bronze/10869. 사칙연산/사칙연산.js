8580df1a664f1030368893ec77d67b4853e07257
YeBin0711 <151003669+YeBin0711@users.noreply.github.com>
2024-10-13 17:28:13 +0900

    [Bronze V] Title: 사칙연산, Time: 108 ms, Memory: 9732 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,17 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [];
+
+readline.on('line', function(line) {
+    input = line.split(' ').map(el => parseInt(el));
+}).on('close', function(){
+    console.log(input[0]+input[1]);
+    console.log(input[0]-input[1]);
+    console.log(input[0]*input[1]);
+    console.log(Math.floor(input[0]/input[1]));
+    console.log(input[0]%input[1]);
+    process.exit();
+});
