b6777d61d83c639400480bba5d32bb58b96245a0
YeBin0711 <151003669+YeBin0711@users.noreply.github.com>
2024-10-28 15:56:52 +0900

    [Bronze III] Title: A+B - 6, Time: 100 ms, Memory: 9592 KB -BaekjoonHub

--- 백준/Bronze/10953. A＋B － 6/A＋B － 6.js
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.js
@@ -1,18 +1,8 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-let input = [];
-
-readline.on('line', function(line) {
-    input.push(line.split(',').map(el => parseInt(el)));
-}).on('close', function(){
-    const n = input[0][0];
-    const numbers = input.slice(1);
-    for(let i = 0; i<n; i++){
-        const answer = numbers[i][0]+numbers[i][1];
-        console.log(answer);
-    }
-    process.exit();
-});
+const n = parseInt(input[0]);
+for (let i = 1; i <= n; i++) {
+    const [a, b] = input[i].split(',').map(Number);
+    console.log(a + b);
+}
