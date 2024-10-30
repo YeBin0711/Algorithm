c628f8b815afc822490acb3c0566d4c8d3209ff6
YeBin0711 <151003669+YeBin0711@users.noreply.github.com>
2024-10-30 21:12:14 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 96 ms, Memory: 9372 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
@@ -6,27 +6,15 @@ let count = 0;
 
 for (let i = 1; i <= n; i++) {
     const word = input[i];
-    const indexMap = {};
     let isGroupWord = true;
+    const seenChars = new Set();
 
-    word.split('').forEach((char, idx) => {
-        if (!indexMap[char]) {
-            indexMap[char] = [];
+    for (let j = 0; j < word.length; j++) {
+        if (seenChars.has(word[j]) && word[j] !== word[j - 1]) {
+            isGroupWord = false;
+            break;
         }
-        indexMap[char].push(idx);
-    });
-
-    for (let key in indexMap) {
-        const indices = indexMap[key];
-
-        for (let j = 1; j < indices.length; j++) {
-            if (indices[j] - indices[j - 1] > 1) {
-                isGroupWord = false;
-                break;
-            }
-        }
-
-        if (!isGroupWord) break;
+        seenChars.add(word[j]);
     }
 
     if (isGroupWord) {
